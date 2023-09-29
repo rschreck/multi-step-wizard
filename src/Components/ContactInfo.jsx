7cc5ec1f0f661498d7b568657cd2b2997ca0a29e
Renuka Schreck <schrer3@gmail.com>
2023-09-29 14:04:53 -0500

    //eslint errors

--- src/Components/ContactInfo.jsx
+++ src/Components/ContactInfo.jsx
@@ -54,6 +54,7 @@ function ContactInfo() {
 
   const validateEmail = (email) => {
     return email.match(
+      //eslint-disable-next-line
       /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     );
   };
