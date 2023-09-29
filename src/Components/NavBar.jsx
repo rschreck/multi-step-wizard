7e502fe8c58f4ff9da907b3614a09706a48759c2
Renuka Schreck <schrer3@gmail.com>
2023-09-29 10:38:30 -0500

    Nav Link disable condition

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { useStep } from "./context";
 
 const Navbar = () => {
   const [showNavbar, setShowNavbar] = useState(false);
-
+  const [state] = useStep();
   return (
     <nav className="navbar">
       <div className="container">
@@ -16,10 +17,20 @@ const Navbar = () => {
               <NavLink to="/user">User</NavLink>
             </li>
             <li>
-              <NavLink to="/contact">Contact</NavLink>
+              <NavLink
+                className={state?.["step1"] ? "" : "disabled-link"}
+                to="/contact"
+              >
+                Contact
+              </NavLink>
             </li>
             <li>
-              <NavLink to="/address">Address</NavLink>
+              <NavLink
+                className={state?.["step2"] ? "" : "disabled-link"}
+                to="/address"
+              >
+                Address
+              </NavLink>
             </li>
           </ul>
         </div>
@@ -27,46 +38,5 @@ const Navbar = () => {
     </nav>
   );
 };
-
-// const StepWizard = () => {
-//   const [currentStep, setCurrentStep] = useState(0);
-
-//   return (
-//     <div>
-//       <h1>Step Wizard</h1>
-//       <ul>
-//         <li>
-//           <button
-//             className={currentStep === 1 ? "w3-button w3-green" : "w3-button "}
-//             onClick={() => setCurrentStep(1)}
-//           >
-//             User Info
-//           </button>
-//         </li>
-//         <li>
-//           <button
-//             className={currentStep === 2 ? "w3-button w3-green" : "w3-button"}
-//             onClick={() => setCurrentStep(2)}
-//           >
-//             Contact Info
-//           </button>
-//         </li>
-//         <li>
-//           <button
-//             className={currentStep === 3 ? "w3-button w3-green" : "w3-button"}
-//             onClick={() => setCurrentStep(3)}
-//           >
-//             Address 3
-//           </button>
-//         </li>
-//       </ul>
-//       <div>
-//         {currentStep === 1 && <UserInfo />}
-//         {currentStep === 2 && <ContactInfo />}
-//         {currentStep === 3 && <AddressInfo />}
-//       </div>
-//     </div>
-//   );
-// };
 
 export { Navbar };
